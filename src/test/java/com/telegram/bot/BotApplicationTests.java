e3154e7a9c4a10280f427d90a05126bce655bab2
ivan <ivan.alekseevich.popov@gmail.com>
2024-05-28 17:00:42 +0300

    added context loading test

--- src/test/java/com/telegram/bot/BotApplicationTests.java
+++ src/test/java/com/telegram/bot/BotApplicationTests.java
@@ -1,14 +1,26 @@
 package com.telegram.bot;
 
+import com.telegram.bot.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-//@SpringBootTest
+@SpringBootTest()
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class}
+)
 class BotApplicationTests {
+	@MockBean
+	private UserRepository userRepository;
 
-//	@Test
+	@Test
 	void contextLoads() {
-
 	}
 
 }
